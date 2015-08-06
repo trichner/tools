9a97076ac67de56738ad36bf638dc9ee545fe232
Josef Ziegler <ippeb24@gmail.com>
2015-08-06 20:28:14 +0200

    Added jochef's asciitable to tools.

---
+++ asciitable/asciitable.c
@@ -0,0 +1,24 @@
+// Generates the ASCII table of printable characters.
+#include <stdio.h>
+
+int main(){
+  const char A[]="*        THE ASCII TABLE BY JOSEF ZIEGLER         *";
+  int i = -1, j = 0;
+  while(j < 3) {
+    while(++i < sizeof(A)-1) { // because of the '\0'
+      if(j%2 == 0)
+	putc('*', stdout);
+      else
+	putc(*(A+i), stdout);}
+    putc('\n', stdout);
+    j++;
+    i = -1;
+  }
+  for(i = 33; i < 127; i++){
+    printf("%.3d ", i);
+    putc(i, stdout);
+    printf("  ");
+    if (i % 8==0)
+      putc(10, stdout);
+  }
+}
